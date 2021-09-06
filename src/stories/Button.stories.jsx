3c86fc572290b0d7f77302e8399c810f5cd476fa
therealsharath <palathingal@gatech.edu>
2021-09-06 04:08:08 -0400

    Removed unwanted boiler code

--- src/stories/Button.stories.jsx
+++ src/stories/Button.stories.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-import { Button } from './Button';
+import { Button } from './components/Button';
 
 export default {
-  title: 'Example/Button',
+  title: 'Components/Button',
   component: Button,
   argTypes: {
     backgroundColor: { control: 'color' },
