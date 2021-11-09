97400aa27965af30e52afe9d4bd2f142d594a0a4
Eric M <matthews.eric02@gmail.com>
2021-11-09 17:44:22 -0500

    added changes for most of the components

--- src/stories/Alert.stories.jsx
+++ src/stories/Alert.stories.jsx
@@ -6,17 +6,95 @@ export default {
   title: 'Components/Alert',
   component: Alert,
   argTypes: {
+    iconType: {
+      options: ['success', 'failure', 'message', 'brand message'],
+      control: {type: 'radio'},
+    },
     backgroundColor: { control: 'color' },
+    size: {
+      options: ['large', 'regular'],
+      control: {type: 'radio'},
+    }
   },
 };
 
 const Template = (args) => <Alert {...args} />;
 
-export const Primary = Template.bind({});
-Primary.args = {
-  backgroundColor: 'rgba(205, 242, 214, 1)',
-  alertText: 'Nice, you triggered this alert message!',
-  url: 'https://youtu.be/dQw4w9WgXcQ'
+export const SuccessAlert = Template.bind({});
+SuccessAlert.args = {
+  iconType: 'success',
+  iconColor: '#13B461',
+  backgroundColor: '#E3FCEF',
+  size: 'regular',
+  hasButton: false,
+  buttonLabel: 'Button',
+  hasHeader: false,
+  headerLabel: "I'M A SUCCESS!",
+  alertText: 'This is a description of something that has happened + what you can do.',
 };
 
+export const FailureAlert = Template.bind({});
+FailureAlert.args = {
+  iconType: 'failure',
+  iconColor: '#FF3939',
+  backgroundColor: '#FFE3E3',
+  size: 'regular',
+  hasButton: false,
+  buttonLabel: 'Button',
+  hasHeader: false,
+  headerLabel: "ERROR NO FILE FOUND",
+  alertText: 'This is a description of something that has happened + what you can do.',
+};
 
+export const MessageAlert = Template.bind({});
+MessageAlert.args = {
+  iconType: 'message',
+  iconColor: '#005EEF',
+  backgroundColor: '#DEEBFF',
+  size: 'regular',
+  hasButton: false,
+  buttonLabel: 'Button',
+  hasHeader: false,
+  headerLabel: "HEY THERE",
+  alertText: 'This is a description of something that has happened + what you can do.',
+};
+
+export const BrandMessageAlert = Template.bind({});
+BrandMessageAlert.args = {
+  iconType: 'brand message',
+  iconColor: '#473F91',
+  backgroundColor: '#EAE6FF',
+  size: 'regular',
+  hasButton: false,
+  buttonLabel: 'Button',
+  hasHeader: false,
+  headerLabel: "WE BOG HERE ",
+  alertText: 'This is a description of something that has happened + what you can do.',
+};
+
+// export const FailureAlert = Template.bind({});
+// FailureAlert.args = {
+//   backgroundColor: '#FFE3E3',
+//   hasButton: false,
+//   buttonLabel: 'Button',
+//   alertText: 'Nice, you triggered this alert message!',
+//   url: 'https://youtu.be/dQw4w9WgXcQ'
+// };
+
+// export const AlertMessage = Template.bind({});
+// AlertMessage.args = {
+//   backgroundColor: '#DEEBFF',
+//   hasButton: false,
+//   buttonLabel: 'Button',
+//   alertText: 'Nice, you triggered this alert message!',
+//   url: 'https://youtu.be/dQw4w9WgXcQ'
+// };
+
+// export const AlertBrandMessage = Template.bind({});
+// AlertBrandMessage.args = {
+//   backgroundColor: '#EAE6FF',
+//   hasButton: false,
+//   buttonLabel: 'Button',
+//   alertText: 'Nice, you triggered this alert message!',
+//   url: 'https://youtu.be/dQw4w9WgXcQ'
+// };
--- src/stories/components/Alert.jsx
+++ src/stories/components/Alert.jsx
@@ -1,43 +1,294 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import '../css/alert.css';
+import { Button } from './Button.jsx';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faInfoCircle, faExclamationTriangle, faCheckCircle, faTimes } from '@fortawesome/free-solid-svg-icons'
 
 /**
  * Primary UI component for user interaction
  */
- export const Alert = ({backgroundColor, alertText, url, ...props }) => {
-    return (
-      <div
-        type="alert"
-        className={['storybook-alert', `storybook-alert--${url}`].join(' ')}
-        style={backgroundColor && { backgroundColor }}
-        {...props}
-      >
-        <a href={url}>{(alertText == null || alertText === '') ? url : alertText}</a>
-        <button style={backgroundColor && { backgroundColor }}>
-            <img src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/285/orangutan_1f9a7.png" alt='exit alert'></img>
-        </button>
-      </div>
-    );
-  };
+
+/**
+ * Props: backgroundColor: str, borderColor: str, alertText: str, 
+ * hasButton: bool + buttonText: str, hasHeader: bool + headerText: str, size: str, textColor: str
+ */
+ export const Alert = ({iconType, iconColor, backgroundColor, hasButton, buttonLabel, hasHeader, headerLabel, alertText, url, size, ...props }) => {
+
+  // var style = "";
+  // style = "backgroundColor=" + backgroundColor + " border=1px solid " + border;
+  var alertIcon; 
+  if (iconType === 'success') {
+    alertIcon = faCheckCircle
+  } else if (iconType === 'failure') {
+    alertIcon = faExclamationTriangle
+  } else {
+    alertIcon = faInfoCircle
+  }
+
+  if (hasButton) {
+    if (hasHeader) {
+      if (size === 'large') {
+        return (
+          <div
+            type="alert"
+            className='storybook-alert-large-header-button'
+            style={backgroundColor && { backgroundColor }} 
+            {...props}
+          >
+            <div className="header">
+              <div className="headerContent"> 
+                <FontAwesomeIcon icon= {alertIcon} className="icon"/> 
+              <p>{headerLabel}</p>
+              </div>
+
+              <div className="x"> 
+                <FontAwesomeIcon icon={faTimes} className="xIcon"/> 
+              </div>
+
+            </div>
+            <div className="alertBodyWithHeader">
+              <p>{alertText}</p>
+              <Button variant="tertiary" label="Button"/>
+            </div>
+          </div>
+        );
+      } else {
+        return (
+          <div
+            type="alert"
+            className='storybook-alert-header-button'
+            style={backgroundColor && { backgroundColor }}
+            {...props}
+          >
+            <div className="header">
+              <div className="headerContent"> 
+                <FontAwesomeIcon icon= {alertIcon} className="icon"/> 
+              <p>{headerLabel}</p>
+              </div>
+
+              <div className="x"> 
+                <FontAwesomeIcon icon={faTimes} className="xIcon"/> 
+              </div>
+
+            </div>
+
+            <div className="alertBodyWithHeader">
+              <p>{alertText}</p>
+              <Button variant="tertiary" label="Button"/>
+            </div>
+          </div>
+        );
+      }
+    } else {
+      if (size === 'large') {
+        return (
+          <div
+            type="alert"
+            className='storybook-alert-large-button'
+            style={backgroundColor && { backgroundColor }}
+            {...props}
+          >
+            <div className="alertBodyLarge">
+              <FontAwesomeIcon icon= {alertIcon} className="icon"/> 
+
+              <p>{alertText}</p>
+              <Button variant="tertiary" label="Button"/>
+              <div className="x"> 
+                <FontAwesomeIcon icon={faTimes} className="xIcon"/> 
+              </div>
+            </div>
+          </div>
+        );
+      } else {
+        return (
+          <div
+            type="alert"
+            className='storybook-alert-button'
+            style={backgroundColor && { backgroundColor }}
+            {...props}
+          >
+            <div className="alertBody">
+              <FontAwesomeIcon icon={alertIcon} className="icon"/> 
+
+              <p>{alertText}</p>
+              <Button variant="tertiary" label="Button"/>
+
+              <div className="x"> 
+                <FontAwesomeIcon icon={faTimes} className="xIcon"/> 
+              </div>
+            </div>
+          </div>
+        );
+      }
+    }
+  } else {
+    if (hasHeader) {
+      if (size === 'large') {
+        return (
+          <div
+            type="alert"
+            className='storybook-alert-large-header'
+            style={backgroundColor && { backgroundColor }}
+            {...props}
+          >
+            <div className="header">
+              <div className="headerContent"> 
+                <FontAwesomeIcon icon= {alertIcon} className="icon"/> 
+              <p>{headerLabel}</p>
+              </div>
+
+              <div className="x"> 
+                <FontAwesomeIcon icon={faTimes} className="xIcon"/> 
+              </div>
+            </div>
+            <div className="alertBodyWithHeader">
+              <p>{alertText}</p>
+            </div>
+          </div>
+        );
+      } else {
+        return (
+          <div
+            type="alert"
+            className='storybook-alert-header'
+            style={backgroundColor && { backgroundColor }}
+            {...props}
+          >
+            <div className="header">
+              <div className="headerContent"> 
+                <FontAwesomeIcon icon= {alertIcon} className="icon"/> 
+              <p>{headerLabel}</p>
+              </div>
+
+              <div className="x"> 
+                <FontAwesomeIcon icon={faTimes} className="xIcon"/> 
+              </div>
+            </div>
+            <div className="alertBodyWithHeader">
+              <p>{alertText}</p>
+            </div>
+          </div>
+        );
+      }
+    } else {
+      if (size === 'large') {
+        return (
+          <div
+            type="alert"
+            className='storybook-alert-large'
+            style={backgroundColor && { backgroundColor }} 
+            {...props}
+          >
+            <div className="alertBodyLarge">
+              <FontAwesomeIcon icon={alertIcon} className="icon"/> 
+              <p>{alertText}</p>
+              <div className="x"> 
+                <FontAwesomeIcon icon={faTimes} className="xIcon"/> 
+              </div>
+            </div>
+          </div>
+        );
+      } else {
+        return (
+          <div
+            type="alert"
+            className='storybook-alert'
+            style={backgroundColor && { backgroundColor }}
+            {...props}
+          >
+
+            <div className="alertBody">
+              <FontAwesomeIcon icon={alertIcon} className="icon" color={iconColor}/> 
+
+              <p>{alertText}</p>
+              <div className="x"> 
+                <FontAwesomeIcon icon={faTimes} className="xIcon"/> 
+              </div>
+            </div>
+          </div>
+        );
+      }
+    }
+  }
+};
   
   Alert.propTypes = {
+
+    /**
+     * What type of alert
+     */
+     iconType: PropTypes.string,
+
+    /**
+    * What type of alert
+    */
+    iconColor: PropTypes.string,
+
     /**
      * What background color to use
      */
     backgroundColor: PropTypes.string,
+
+    // /**
+    //  * What background color to use
+    //  */
+    // border: PropTypes.string,
+
+    /**
+     * Boolean value for whether the alert has a button or not.
+     */
+    hasButton: PropTypes.bool,
+
+    /**
+     * Label of the button (if it has one)
+     */
+    buttonLabel: PropTypes.string,
+
+    /**
+     * Boolean value for whether the alert has a button or not.
+     */
+     hasHeader: PropTypes.bool,
+
+     /**
+      * Label of the button (if it has one)
+      */
+     headerLabel: PropTypes.string,
+
     /**
      * Alert message
      */
     alertText: PropTypes.string,
-    /**
-     * Alert link
-     */
-    url: PropTypes.string.isRequired,
+
   };
   
   Alert.defaultProps = {
-    backgroundColor: 'rgba(205, 242, 214, 1)',
-    alertText: 'Nice, you triggered this alert message!', 
-    url: 'https://youtu.be/dQw4w9WgXcQ',
+    iconType: 'success',
+    iconColor: '#13B461',
+    backgroundColor: '#E3FCEF',
+    size: 'regular',
+    hasButton: false,
+    hasHeader: false,
+    buttonLabel: 'Button',
+    alertText: 'This is a description of something that has happened + what you can do.', 
   };
+
+
+
+
+
+
+  // temporarily removed code
+
+  // return (
+  //   <div
+  //     type="alert"
+  //     className='storybook-alert'
+  //     // className={['storybook-alert', `storybook-alert--${url}`].join(' ')}
+  //     style={backgroundColor && { backgroundColor }}
+  //     {...props}
+  //   >
+  //     <a href={url}>{(alertText == null || alertText === '') ? url : alertText}</a>
+  //     <Button variant="tertiary" label="Button"/>
+  //   </div>
+  // );
