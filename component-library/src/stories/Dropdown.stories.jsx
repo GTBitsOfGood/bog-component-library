9b6a3471760f7914b366edbf2b217c72ea19e888
Yaritza Marlene Garcia Gomez <ygomez9@gatech.edu>
2021-11-16 16:31:11 -0500

    added figma files to dropdown and input components

--- component-library/src/stories/Dropdown.stories.jsx
+++ component-library/src/stories/Dropdown.stories.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { Dropdown } from './components/Dropdown';
+import { withDesign } from 'storybook-addon-designs'
 
 export default {
   title: 'Components/Dropdown',
@@ -19,6 +20,12 @@ export default {
         control: {type: 'radio'},
     },
   },
+  parameters : {
+    design: {
+      type: "figma",
+      url: "https://www.figma.com/file/DEfDgRT4G9732YxrqTqq8V/BoG-Component-Library-(Design-System)-%2F-Fall21?node-id=1042%3A13616"
+    },
+  }
 };
 
 const Template = (args) => <Dropdown {...args} />;
