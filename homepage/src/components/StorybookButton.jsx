18655ce8f317c8d03892eb316f51f941c9836257
Sharath Palathingal <palathingal@gatech.edu>
2021-11-16 12:35:45 -0600

    Deployed storybook

--- homepage/src/components/StorybookButton.jsx
+++ homepage/src/components/StorybookButton.jsx
@@ -4,7 +4,7 @@ import logo from '../assets/storybook_logo.svg';
 function Button() {
   return (
       <a 
-        href="/"
+        href="https://6193f7d7f0559c003a605d4f-ysjolyadgi.chromatic.com/"
         alt="link_to_storybook"
         className="storybook-button"
         target="_blank"
