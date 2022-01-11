02e21d36e78c4ff51dccbcf35b280885eb4a1d12
Sharath Palathingal <palathingal@gatech.edu>
2022-01-12 03:20:02 +0400

    Moved drawer to a separate component

---
+++ homepage/src/components/LibraryDrawer.jsx
@@ -0,0 +1,14 @@
+import '../css/LibraryDrawer.css';
+
+function LibraryComponents() {
+  return (
+    <div className="components-menubar">
+        <span className="menubar-heading">Foundation</span>
+        <a href="https://www.figma.com/file/DEfDgRT4G9732YxrqTqq8V/BoG-Design-System-(Working-File)-%2F-Fall21?node-id=760%3A3320" className="menubar-content" alt="color-link" target="_blank" rel="noreferrer">Color</a>
+        <a href="https://www.figma.com/file/DEfDgRT4G9732YxrqTqq8V/BoG-Design-System-(Working-File)-%2F-Fall21?node-id=646%3A896" className="menubar-content" alt="typography-link" target="_blank" rel="noreferrer">Typography</a>
+        <span className="menubar-heading">Components</span>
+    </div>
+  );
+}
+
+export default LibraryComponents;
