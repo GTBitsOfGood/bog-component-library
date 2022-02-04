17d59ebd7cb817c3a56ecf8a280bf537c7d6e560
Fattimo <matt.chen522@gmail.com>
2022-02-04 18:57:25 -0500

    more comprehensive test example added

---
+++ component-library/src/stories/Dropdown.test.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+import {
+  render,
+  cleanup,
+  fireEvent,
+} from '@testing-library/react';
+import { composeStories } from '@storybook/testing-react';
+import * as stories from './Dropdown.stories';
+
+describe('Dropdown', () => {
+  afterEach(() => {
+    cleanup()
+  })
+
+  const { Regular } = composeStories(stories)
+
+  it('should create a dropdown', () => {
+    const { getByText, getAllByRole } = render(<Regular />)
+
+    expect(getByText('Placeholder text')).toBeInTheDocument()
+    expect(getAllByRole('button').length).toBe(1)
+  })
+
+  it('should be dropped down after clicking', () => {
+    const { getByText, getAllByRole, getByAltText } = render(<Regular />)
+
+    const dropdown = getByText('Placeholder text')
+    expect(getByAltText('open')).toBeInTheDocument()
+    fireEvent.click(dropdown)
+    expect(getByAltText('close')).toBeInTheDocument()
+    expect(getByText('Sharath')).toBeInTheDocument()
+    expect(getAllByRole('button').length).toBe(7)
+  })
+})
