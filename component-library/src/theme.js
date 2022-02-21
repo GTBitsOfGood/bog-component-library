7d78722203bd32a7d730ed895f9831deab8f3bc4
Sharath Palathingal <palathingal@gatech.edu>
2022-02-21 07:51:04 -0500

    Added typography

--- component-library/src/theme.js
+++ component-library/src/theme.js
@@ -14,7 +14,7 @@ const midnightPalette = {
     400: baseColors.midnight[2],
     500: baseColors.midnight[3],
     700: baseColors.midnight[4],
-}
+};
 
 const orangePalette = {
     100: baseColors.orange[0],
@@ -22,7 +22,7 @@ const orangePalette = {
     400: baseColors.orange[2],
     500: baseColors.orange[3],
     700: baseColors.orange[4],
-}
+};
 
 const yellowPalette = {
     100: baseColors.yellow[0],
@@ -30,13 +30,13 @@ const yellowPalette = {
     400: baseColors.yellow[2],
     500: baseColors.yellow[3],
     700: baseColors.yellow[4],
-}
+};
 
 const secondaryColors = {
     green: ['#53ED9D', '#17D874', '#17D874', '#13B461', '#0F904D'],
     red: ['#FF8E8E', '#FF7171', '#FF5555', '#FF3939', '#C63636'],
     blue: ['#5698FF', '#3484FF', '#126FFF', '#005EEF', '#0051CC']
-}
+};
 
 const greenPalette = {
     100: secondaryColors.green[0],
@@ -44,7 +44,7 @@ const greenPalette = {
     400: secondaryColors.green[2],
     500: secondaryColors.green[3],
     700: secondaryColors.green[4],
-}
+};
 
 const redPalette = {
     100: secondaryColors.red[0],
@@ -52,7 +52,7 @@ const redPalette = {
     400: secondaryColors.red[2],
     500: secondaryColors.red[3],
     700: secondaryColors.red[4],
-}
+};
 
 const bluePalette = {
     100: secondaryColors.blue[0],
@@ -60,13 +60,13 @@ const bluePalette = {
     400: secondaryColors.blue[2],
     500: secondaryColors.blue[3],
     700: secondaryColors.blue[4],
-}
+};
 
 
 const typographyColors = {
     light: ['#FFFFFF', '#F2F2F2', '#E5E5E5', '#D9D9D9', '#CCCCCC'],
     dark: ['#BFBFBF', '#8C8C8C', '#666666', '#3F3F3F', '#000000']
-}
+};
 
 const lightPalette = {
     100: typographyColors.light[0],
@@ -74,7 +74,7 @@ const lightPalette = {
     400: typographyColors.light[2],
     500: typographyColors.light[3],
     700: typographyColors.light[4],
-}
+};
 
 const darkPalette = {
     100: typographyColors.dark[0],
@@ -82,7 +82,7 @@ const darkPalette = {
     400: typographyColors.dark[2],
     500: typographyColors.dark[3],
     700: typographyColors.dark[4],
-}
+};
 
 const colors = {
     ...baseColors,
@@ -114,4 +114,165 @@ const colors = {
         secondary: orangePalette,
         tertiary: bluePalette,
     }
-}
+};
+
+const fonts = {
+    main: fontStack(['VisbyCF']),
+    secondary: fontStack(['OpenSans']),
+};
+
+const fontWeights = {
+    thin: 100,
+    light: 300,
+    regular: 400,
+    medium: 500,
+    semibold: 600,
+    bold: 700,
+    extrabold: 800,
+    heavy: 900,
+};
+
+const letterSpacings = {
+    five: '0.05em',
+    zero: '0',
+    negOne: '-0.01em',
+    negTwo: '-0.02em',
+};
+
+  
+const typography = {
+    // Display
+    display: {
+        fontFamily: fonts.primary,
+        fontWeight: fontWeights.extrabold,
+        fontSize: '64px',
+        letterSpacing: letterSpacings.negOne,
+        lineHeight: '76.8px',
+    },
+    display2: {
+        fontFamily: fonts.primary,
+        fontWeight: fontWeights.bold,
+        fontSize: '56px',
+        letterSpacing: letterSpacings.negOne,
+        lineHeight: '67.2px',
+    },
+    
+    // Heading
+    h1: {
+        fontFamily: fonts.primary,
+        fontWeight: fontWeights.extrabold,
+        fontSize: '40px',
+        letterSpacing: letterSpacings.negOne,
+        lineHeight: '48px',
+    },
+    h2: {
+        fontFamily: fonts.primary,
+        fontWeight: fontWeights.extrabold,
+        fontSize: '32px',
+        letterSpacing: letterSpacings.negOne,
+        lineHeight: '38px',
+    },
+    h3: {
+        fontFamily: fonts.primary,
+        fontWeight: fontWeights.extrabold,
+        fontSize: '24px',
+        letterSpacing: letterSpacings.negOne,
+        lineHeight: '29px',
+    },
+    h4: {
+        fontFamily: fonts.primary,
+        fontWeight: fontWeights.bold,
+        fontSize: '24px',
+        letterSpacing: letterSpacings.negOne,
+        lineHeight: '36px',
+    },
+    
+    // Lead
+    lead: {
+        fontFamily: fonts.secondary,
+        fontWeight: fontWeights.bold,
+        fontSize: '16px',
+        letterSpacing: letterSpacings.negTwo,
+        lineHeight: '21.79px',
+    },
+
+    // Body
+    body: {
+        fontFamily: fonts.secondary,
+        fontWeight: fontWeights.regular,
+        fontSize: '20px',
+        lineHeight: '26px',
+    },
+    bodySmall: {
+        fontFamily: fonts.secondary,
+        fontWeight: fontWeights.regular,
+        fontSize: '16px',
+        letterSpacing: letterSpacings.negTwo,
+        lineHeight: '21px',
+    },
+
+    capitalized: {
+        fontFamily: fonts.primary,
+        fontWeight: fontWeights.extrabold,
+        fontSize: '24px',
+        letterSpacing: letterSpacings.five,
+        lineHeight: '28.8px',
+        textTransform: 'uppercase',
+    },
+    capitalizedSmall: {
+        fontFamily: fonts.primary,
+        fontWeight: fontWeights.extrabold,
+        fontSize: '8px',
+        letterSpacing: letterSpacings.five,
+        lineHeight: '10px',
+        textTransform: 'uppercase',
+    },
+
+    // Caption
+    caption: {
+        fontFamily: fonts.secondary,
+        fontWeight: fontWeights.regular,
+        fontSize: '12px',
+        lineHeight: '16px',
+    },
+
+    // Label Text
+    largeLabel: {
+        fontFamily: fonts.primary,
+        fontWeight: fontWeights.regular,
+        fontSize: '16px',
+        lineHeight: '24px',
+    },
+    mediumLabel: {
+        fontFamily: fonts.primary,
+        fontWeight: fontWeights.regular,
+        fontSize: '14px',
+        lineHeight: '20px',
+    },
+    smallLabel: {
+        fontFamily: fonts.primary,
+        fontWeight: fontWeights.regular,
+        fontSize: '12px',
+        lineHeight: '16px',
+    },
+
+    // Button
+    button: {
+        fontFamily: fonts.primary, 
+        fontWeight: fontWeights.extrabold,
+        fontSize: '16px',
+        lineHeight: '23px',
+    },
+    buttonLarge: {
+        fontFamily: fonts.primary, 
+        fontWeight: fontWeights.extrabold,
+        fontSize: '18px',
+        lineHeight: '25px',
+    },
+    buttonSmall: {
+        fontFamily: fonts.primary, 
+        fontWeight: fontWeights.extrabold,
+        fontSize: '14px',
+        lineHeight: '19px',
+    }
+  };
