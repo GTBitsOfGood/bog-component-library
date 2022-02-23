68f7232222d32a59d267de05f8fa2d5631c0f071
Jade <jadelaw71700@hotmail.com>
2022-02-22 22:21:51 -0500

    radioButton console error

--- component-library/src/components/RadioButton/RadioButton.jsx
+++ component-library/src/components/RadioButton/RadioButton.jsx
@@ -7,10 +7,11 @@ import './radio-button.css';
     if (isDisabled) {
         disableStyle = "disable-button";
     }
+    const handleChange = () => {}
     
     return(
         <label className={`radio-container ${disableStyle}`}>
-            <input type="radio" id={id} name={groupName} value={value} disabled={isDisabled} checked={isChecked}/>
+            <input type="radio" id={id} name={groupName} value={value} disabled={isDisabled} checked={isChecked} onChange={handleChange}/>
             <span className={`selection ${disableStyle}`}></span>
             <p className={disableStyle != "" ? `${disableStyle}-text` : `labelText`}>{label}</p>
         </label>
